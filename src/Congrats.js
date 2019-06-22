53ef76cc8463ce170169e22e8f9d30099601b79b
Edwin <Edwin.Martinez@people10.com>
2019-06-21 23:14:18 -0400

    first commit

---
+++ src/Congrats.js
@@ -0,0 +1,21 @@
+import React from 'react';
+/**
+ * @function
+ * @param {object}
+ */
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test="component-congrats" />
+    )
+  }
+  
+}
