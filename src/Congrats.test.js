129de4efd0805faa50fd911861c10b0d8dd14fe2
Edwin <Edwin.Martinez@people10.com>
2019-06-25 18:26:18 -0400

    Set up redux

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -27,7 +27,7 @@ test("renders no text when `success` prop is false", () => {
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
-test("renders non-empty congrats message when `success` props is true", () => {
+  test("renders non-empty congrats message when `success` props is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
@@ -36,4 +36,4 @@ test("renders non-empty congrats message when `success` props is true", () => {
 test("does  not throw warning with expected props", () => {
   const expectedProps = { success: false };
   checkProps(Congrats, expectedProps);
-});
+})
