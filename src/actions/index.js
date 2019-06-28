6265f180b1217b49bd925a6ed45fea04ea21fcc6
Edwin <Edwin.Martinez@people10.com>
2019-06-27 22:14:16 -0400

    Add secretWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
+import axios from 'axios';
+
 import {getLetterMatchCount} from '../helpers';
+
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-    GUESS_WORD: 'GUESS_WORD'
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD'
   };
   
   ///**SEE index.test.js for why it's commented out */
@@ -35,4 +39,16 @@ export const guessWord = (guessedWord) => {
 
   }
 };
+
+export const getSecretWord = () => {
+  return (dispatch, getState) => {
+    return axios.get('http://localhost:3030')
+      .then(response => {
+        dispatch({
+          type: actionTypes.SET_SECRET_WORD, 
+          payload: response.data
+        })
+      });
+  }
+}
 
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,19 +1,43 @@
-//here for testing action creators
-//although an argument can be made to skip testing action creators
-//and only do integration tests with state
-//it's a reminder not to test implementation details
-//it's just here to show you how to test action creators
+import moxios from 'moxios';
+import { storeFactory } from '../../test/testUtils';
+import { getSecretWord } from './';
 
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    //tells axios to use moxios for all of it's requests
+    moxios.install()
+  });
+  afterEach(() => {
+    // return axios to it's original state
+    moxios.uninstall()
+  });
 
-import {correctGuess, actionTypes}  from './';
+  test('adds response word to state', () => {
+    const secretWord = 'party';
+    const store = storeFactory();
 
-describe('correctGuess', () => {
-  // test('returns an action with type `CORRECT_GUESS`', () => {
-  //   const action = correctGuess();
-  //   //.toBe will fail we need to use toEqual to compare objects
-  //   expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
-  // });
-  test('just here as a placeholder', () => {
-    
-  })
-}); 
+    //wait(), tells moxios how to respond when axios sends in a request
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status:200,
+        response: secretWord
+      })
+    });
+
+    // dispatch the action using the store
+    //since getSecret word returns a function that returns a PROMISE
+    //then store.dispatch() will return a Promise as well
+    return store.dispatch(getSecretWord())
+      //it will wait to run this calback, until a Promise is resolve
+      .then(()=> {
+        //at this point, we assume the getSecretWord made the request
+        //it came back with a response and saved it in the reducer as a newState
+        //we get the newState and compare it to 'party'
+        const newState = store.getState();
+        expect(newState.secretWord).toBe(secretWord);
+      })
+      //*note, we are return a promise which means it will wait till it resolves 
+      //before completing the test
+  });
+})
